cb3088aaaa7efd8d68bc9d43fc514c06445c6756
Emmanuel <eyapura96@gmail.com>
2022-06-13 23:02:35 -0300

    Agrega express

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.send('Ruta get')
+})
+
+app.get('*', (req,res) => {
+    let { url, method } = req;
+    res.send(`La ruta ${url} metodo ${method} no esta implementada`);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
+})
+
